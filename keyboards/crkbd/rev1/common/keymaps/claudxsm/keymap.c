f102a1d38acb5dd92b794d997d24efa0bec93618
CS <claudia.j.sm@gmail.com>
2020-11-07 10:49:11 -0600

    formatting

--- keyboards/crkbd/rev1/common/keymaps/claudxsm/keymap.c
+++ keyboards/crkbd/rev1/common/keymaps/claudxsm/keymap.c
@@ -30,52 +30,35 @@ enum { TD_QUOT = 0, TD_PAGING, TD_DASH };
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-[_BASE] = LAYOUT_split_3x6_3(  
-    //,----------------------------------------------------.      ,----------------------------------------------.
-        KC_TAB,     KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,           KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,       KC_BSPC,
-    //|------------ +------ +-----  +---``--- +------ +------|        ------ +-----  +------ +------ +---------- +------|
-        KC_LCTL,    KC_A,   KC_S,   KC_D,   LSFT_T(KC_F),   KC_G,     KC_H,   RSFT_T(KC_J),   KC_K,   KC_L,   KC_SCLN,    TD(TD_QUOT),
-    //|------------ +------ +-----  +------ +------ +------|        ------ +-----  +------ +------ +---------- +------|
-        KC_LSFT,    KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,           KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,    KC_RSHIFT,
-    //|------------ +------ +-----  +------ +------ +------|        ------ +-----  +------ +------ +---------- +------|
-        KC_LALT,        MO(_LOWER),         LGUI_T(KC_SPC),         LT(_FN, KC_ENTER),   MO(_RAISE),  KC_CCCV   
-    //,----------------------------------------------------.      ,----------------------------------------------.
-    ),
+[_BASE] = LAYOUT_split_3x6_3( 
+    KC_TAB,    KC_Q, KC_W, KC_E, KC_R,         KC_T,      KC_Y, KC_U,         KC_I,    KC_O,   KC_P,      KC_BSPC,
+    KC_LCTL,   KC_A, KC_S, KC_D, LSFT_T(KC_F), KC_G,      KC_H, RSFT_T(KC_J), KC_K,    KC_L,   KC_SCLN,   TD(TD_QUOT),
+    KC_LSFT,   KC_Z, KC_X, KC_C, KC_V,         KC_B,      KC_N, KC_M,         KC_COMM, KC_DOT, KC_SLSH,   KC_RSHIFT,
+    
+    KC_LALT, MO(_LOWER), LGUI_T(KC_SPC),                  LT(_FN, KC_ENTER), MO(_RAISE), KC_CCCV   
+),
 
 [_LOWER] = LAYOUT_split_3x6_3(  
-    //,----------------------------------------------------.      ,----------------------------------------------.
-        KC_ESC,    XXXXXXX, XXXXXXX, KC_UP, XXXXXXX, XXXXXXX,     XXXXXXX, KC_7,  KC_8,   KC_9,    KC_KP_MINUS,    _______,
-    //|------------ +------ +-----  +------ +------ +------|        |------ +-----  +------ +------ +---------- +------|
-        _______,    XXXXXXX, KC_LEFT, KC_DOWN, KC_RIGHT, XXXXXXX,   XXXXXXX, KC_4,  KC_5,   KC_6,   KC_KP_PLUS,     XXXXXXX,
-    //|------------ +------ +-----  +------ +------ +------|        |------ +-----  +------ +------ +---------- +------|
-        _______,    XXXXXXX, KC_HOME, TD(TD_PAGING), KC_END, XXXXXXX,    XXXXXXX, KC_1,  KC_2,   KC_3,  KC_DOT,        _______,
-    //|------------ +------ +-----  +------ +------ +------|        |------ +-----  +------ +------ +---------- +------|
-        _______,    XXXXXXX,        _______,                        _______,         KC_0,           KC_UNRE
-    //,----------------------------------------------------.      ,----------------------------------------------.
-    ),
+    KC_ESC,    XXXXXXX, XXXXXXX, KC_UP,         XXXXXXX,  XXXXXXX,      XXXXXXX, KC_7, KC_8, KC_9, KC_KP_MINUS,   _______,
+    _______,   XXXXXXX, KC_LEFT, KC_DOWN,       KC_RIGHT, XXXXXXX,      XXXXXXX, KC_4, KC_5, KC_6, KC_KP_PLUS,    XXXXXXX,
+    _______,   XXXXXXX, KC_HOME, TD(TD_PAGING), KC_END,   XXXXXXX,      XXXXXXX, KC_1, KC_2, KC_3, KC_DOT,        _______,
+    
+    _______, XXXXXXX, _______,                                          _______, KC_0, KC_UNRE
+),
 
 [_RAISE] = LAYOUT_split_3x6_3(  
-    //,----------------------------------------------------.      ,----------------------------------------------.
-        KC_TILD,    KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC,        KC_AMPR, KC_ASTR, KC_LCBR, KC_RCBR, KC_BSLS,    KC_DELETE,
-    //|------------ +------ +-----  +------ +------ +------|        |------ +-----  +------ +------ +---------- +------|
-        _______,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   KC_CIRC,  KC_PLUS, TD(TD_DASH),  KC_LPRN, KC_RPRN, KC_COLON,   KC_GRV,
-    //|------------ +------ +-----  +------ +------ +------|        |------ +-----  +------ +------ +---------- +------|
-        KC_CAPS,    XXXXXXX, XXXXXXX, KC_LABK, KC_RABK, XXXXXXX,    KC_UNDS, KC_QUES, KC_LBRC, KC_RBRC, KC_PIPE,    _______,
-    //|------------ +------ +-----  +------ +------ +------|        |------ +-----  +------ +------ +---------- +------|
-        _______,    XXXXXXX,        _______,                         _______,       _______,        KC_SELCUT
-    //,----------------------------------------------------.      ,----------------------------------------------.
-    ),
+    KC_TILD,   KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,        KC_AMPR, KC_ASTR,     KC_LCBR, KC_RCBR, KC_BSLS,  KC_DELETE,
+    _______,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_CIRC,        KC_PLUS, TD(TD_DASH), KC_LPRN, KC_RPRN, KC_COLON, KC_GRV,
+    KC_CAPS,   XXXXXXX, XXXXXXX, KC_LABK, KC_RABK, XXXXXXX,        KC_UNDS, KC_QUES,     KC_LBRC, KC_RBRC, KC_PIPE,  _______,
+   
+    _______, XXXXXXX, _______,                                     _______, _______,     KC_SELCUT
+),
 
 [_FN] = LAYOUT_split_3x6_3(   
-    //,----------------------------------------------------.      ,----------------------------------------------.
-        XXXXXXX,    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,        KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,          
-    //|------------ +------ +-----  +------ +------ +------|          |------ +-----  +------ +------ +---------- +------|
-        _______,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,      KC_VOLU, KC_MPLY, KC_MNXT, XXXXXXX, KC_BRIU,  KC_F12,                
-    //|------------ +------ +-----  +------ +------ +------|          |------ +-----  +------ +------ +---------- +------|
-        RESET,      XXXXXXX, XXXXXXX, XXXXXXX, KC_INSERT, XXXXXXX,    KC_VOLD, KC_MUTE, KC_MPRV, XXXXXXX, KC_BRID, XXXXXXX,  
-    //|------------ +------ +-----  +------ +------ +------|          |------ +-----  +------ +------ +---------- +------|
-        _______,      TG(_LOWER),        _______,                         _______,         XXXXXXX,      _______       
-    //,----------------------------------------------------.      ,----------------------------------------------.
+    XXXXXXX,    KC_F1,   KC_F2,   KC_F3,   KC_F4,     KC_F5,        KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,    KC_F11,   
+    _______,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX,      KC_VOLU, KC_MPLY, KC_MNXT, XXXXXXX, KC_BRIU,   KC_F12, 
+    RESET,      XXXXXXX, XXXXXXX, XXXXXXX, KC_INSERT, XXXXXXX,      KC_VOLD, KC_MUTE, KC_MPRV, XXXXXXX, KC_BRID,   XXXXXXX,  
+    _______, TG(_LOWER), _______,                                   _______, XXXXXXX, _______       
 )
 };
 // clang-format on
