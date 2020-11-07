9e6824b8d0ca7f65d6d0818ccb13e0c9dd6b95c6
CS <claudia.j.sm@gmail.com>
2020-11-07 10:49:28 -0600

    trying different tap config

--- keyboards/crkbd/rev1/common/keymaps/claudxsm/config.h
+++ keyboards/crkbd/rev1/common/keymaps/claudxsm/config.h
@@ -43,5 +43,5 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define OLED_DISABLE_TIMEOUT
 #define TAPPING_TERM 250
 #define TAPPING_TERM_PER_KEY
-#define IGNORE_MOD_TAP_INTERRUPT
+//#define IGNORE_MOD_TAP_INTERRUPT
 #define PERMISSIVE_HOLD
